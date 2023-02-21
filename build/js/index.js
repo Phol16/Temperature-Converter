b3f0760ff7c5e5b89870aad3d2d1336458ac9af4
Phol <Pholibertlim@gmail.com>
2023-02-21 16:50:33 +0800

    feat:UI for tempConverter

---
+++ build/js/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const CF = document.getElementById('CF');
+const FC = document.getElementById('FC');
+const input = document.getElementById('input');
+const result = document.getElementById('resultHere');
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const CF = <HTMLButtonElement>document.getElementById('CF')
+const FC = <HTMLButtonElement>document.getElementById('FC')
+const input = <HTMLInputElement>document.getElementById('input')
+const result = <HTMLSpanElement>document.getElementById('resultHere')
+
+
+
+
+
