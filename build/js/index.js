6f37af48333bf05dfe0090be446ac82ec7ba2b57
Phol <Pholibertlim@gmail.com>
2023-02-23 16:16:51 +0800

    Feat: convertion funcionality

--- build/js/index.js
+++ build/js/index.js
@@ -3,3 +3,45 @@ const CF = document.getElementById('CF');
 const FC = document.getElementById('FC');
 const input = document.getElementById('input');
 const result = document.getElementById('resultHere');
+const error = document.getElementById('error');
+const convert = document.getElementById('convert');
+let selected = "celcius" /* select.celcius */;
+const selectedButton = (a) => {
+    if (a === 'celcius') {
+        CF.style.backgroundColor = 'gray';
+        CF.style.color = 'white';
+        FC.style.backgroundColor = 'white';
+        FC.style.color = 'black';
+        selected = a;
+    }
+    if (a === 'farenheit') {
+        CF.style.backgroundColor = 'white';
+        CF.style.color = 'black';
+        FC.style.backgroundColor = 'gray';
+        FC.style.color = 'white';
+        selected = a;
+    }
+};
+const handleConvert = (a) => {
+    const value = parseInt(input.value);
+    error.textContent = '';
+    if (isNaN(value)) {
+        error.textContent = 'invalid Input';
+        throw new Error('inputted value is not a number');
+    }
+    if (a === 'celcius') {
+        result.textContent = `${(value * (9 / 5)) + 32}`;
+    }
+    else {
+        result.textContent = `${(value - 32) * (5 / 9)}`;
+    }
+};
+CF.addEventListener('click', () => {
+    selectedButton("celcius" /* select.celcius */);
+});
+FC.addEventListener('click', () => {
+    selectedButton("farenheit" /* select.farenheit */);
+});
+convert.addEventListener('click', () => {
+    handleConvert(selected);
+});
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,57 @@ const CF = <HTMLButtonElement>document.getElementById('CF')
 const FC = <HTMLButtonElement>document.getElementById('FC')
 const input = <HTMLInputElement>document.getElementById('input')
 const result = <HTMLSpanElement>document.getElementById('resultHere')
+const error = <HTMLSpanElement>document.getElementById('error')
+const convert = <HTMLButtonElement>document.getElementById('convert')
+
+let selected:string = select.celcius;
+
+const enum select{
+  celcius= 'celcius',
+  farenheit = 'farenheit',
+}
+
+const selectedButton = (a:string):void=>{
+ if(a === 'celcius'){
+  CF.style.backgroundColor = 'gray';
+  CF.style.color = 'white';
+  FC.style.backgroundColor = 'white';
+  FC.style.color = 'black';
+  selected = a;
+ }
+ if(a === 'farenheit'){
+  CF.style.backgroundColor = 'white';
+  CF.style.color = 'black';
+  FC.style.backgroundColor = 'gray';
+  FC.style.color = 'white';
+  selected = a;
+ }
+}
+
+const handleConvert = (a:string) =>{
+  const value:number = parseInt(input.value)
+  error.textContent = ''
+  if(isNaN(value)){
+    error.textContent = 'invalid Input'
+    throw new Error('inputted value is not a number')
+  }
+  if(a === 'celcius'){
+   result.textContent = `${(value *(9/5))+32}`
+  }else{
+  result.textContent = `${(value - 32)*(5/9)}`
+  }
+}
+
+
+CF.addEventListener('click',():void=>{
+  selectedButton(select.celcius)
+} )
+FC.addEventListener('click',():void=>{
+  selectedButton(select.farenheit)
+})
+convert.addEventListener('click', ():void=>{
+  handleConvert(selected)
+})
 
 
 
